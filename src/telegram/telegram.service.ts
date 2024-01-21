5f2d3df3437ffd02d26e1127da4f30921562bb55
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2024-01-21 16:47:16 +0530

    setup telegram bot

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import TelegramBot from 'node-telegram-bot-api';
+import { config } from 'dotenv';
+config();
 
 @Injectable()
-export class TelegramService {}
+export class TelegramService {
+  private bot: TelegramBot;
+  constructor() {
+    this.bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {
+      polling: true,
+    });
+    this.handleCommands();
+  }
+
+  private handleCommands() {
+    this.bot.onText(/\/start/, (msg) => {
+      const chatId = msg.chat.id;
+      this.sendMessage(chatId, 'Welcome to my awesome bot');
+    });
+
+    this.bot.onText(/\/subscribe/, (msg) => {
+      const chatId = msg.chat.id;
+      this.sendMessage(chatId, 'You have been subscribed');
+    });
+
+    this.bot.onText(/\/unsubscribe/, (msg) => {
+      const chatId = msg.chat.id;
+      this.sendMessage(chatId, 'You have been unsubscribe');
+    });
+  }
+
+  sendMessage(chatId: number, message: string) {
+    this.bot.sendMessage(chatId, message);
+  }
+}
