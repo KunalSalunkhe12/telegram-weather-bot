a98deb571e745bdb42f58d3a1b06daeb85a1c9ce
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2024-01-23 15:20:44 +0530

    Add POST method to createAdmin endpoint and enable CORS

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,20 +1,28 @@
-import { Body, Controller, InternalServerErrorException } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  InternalServerErrorException,
+  Post,
+} from '@nestjs/common';
 import { AdminService } from './admin.service';
 
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
+
+  @Post()
   async createAdmin(
     @Body('email') email: string,
     @Body('name') name: string,
     @Body('sub') sub: string,
   ) {
     try {
-      const admin = await this.adminService.createAdmin({ email, name, sub });
+      const admin = await this.adminService.createAdmin(email, name, sub);
       if (admin) {
         return admin;
       }
     } catch (error) {
+      console.log(error);
       throw new InternalServerErrorException(error);
     }
   }
