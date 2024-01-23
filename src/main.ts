344ade84359da33717339961c5f3c6970487bab9
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2024-01-24 00:33:28 +0530

    Add CORS configuration to enable cross-origin requests

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.CLIENT_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type, Authorization',
   });
   await app.listen(3000);
 }
