e0228d140b4ae12c3fda1fe7769ed4b14e9ae3ea
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2024-01-24 00:41:08 +0530

    Add @types/cors package

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
+
+  const corsOptions = {
     origin: process.env.CLIENT_URL,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
     optionsSuccessStatus: 204,
     allowedHeaders: 'Content-Type, Authorization',
-  });
+  };
+
+  app.use(cors(corsOptions));
+
   await app.listen(3000);
 }
 bootstrap();
